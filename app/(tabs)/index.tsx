9d531950923fd02ce539c1302847b3e46dd77170
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-12-12 03:16:26 +0300

    yeni dosyalar eklendi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,42 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 function App() {
-  const message = [
-    {
-      name: 'fafasd',
-      id: 1
-    },
-    {
-      name: 'fgjhfg',
-      id: 2
-    },
-    {
-      name: 'fafajghgsd',
-      id: 3
-    },
-    {
-      name: 'fafaghjghjkghsd',
-      id: 4
-    },
-    {
-      name: 'dfgdgd',
-      id: 5
-    },
-    {
-      name: 'xcvbcvbc',
-      id: 6
-    },
-  ];
+  
+
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={message}
-        renderItem={({ item }) => {
-          return <Text style={styles.content}>{item.name}</Text>;
-        }}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <Text style={styles.title}>Hello World!</Text>
     </View>
   );
 }
@@ -53,14 +23,6 @@ const styles = StyleSheet.create({
     fontSize: 50,
     color: '#fff'
   },
-  content: {
-    fontSize: 30,
-    color: '#fff',
-    flex: 1,
-    backgroundColor: 'darkgrey',
-    marginVertical: 10,
-    padding: 20,
-  }
 })
 
 export default App;
